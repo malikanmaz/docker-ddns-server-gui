063e8ebdf063ef08be58c6be89de3ee94a5bcea9
malikanmaz <mehmet-ali@live.com>
2024-06-12 14:20:21 +0200

    Update ip.go

--- dyndns/nswrapper/ip.go
+++ dyndns/nswrapper/ip.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/benjaminbear/docker-ddns-server/dyndns/ipparser"
+	"github.com/malikanmaz/docker-ddns-server-gui/dyndns/ipparser"
 )
 
 // GetIPType finds out if the IP is IPv4 or IPv6
